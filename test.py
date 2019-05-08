34128ee2f460b0f66f8b6e539903410a0ce12030
DigitalHubRocks <dstadh17@gmail.com>
2019-05-08 18:37:15 +0800

    training and testing for new small comp set

--- test.py
+++ test.py
@@ -1,25 +1,55 @@
+import os
+
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
-from mobilenetv2 import relu6
-import tensorflow as tf
+# from mobilenetv2 import relu6
+# import tensorflow as tf
+
+def num_jpgs_nested_in_folder( folder ):
+    total_imgs = 0
+    for _,_,imgs in os.walk( folder ):
+        total_imgs += len([im for im in imgs if im.endswith(('.jpg','.png'))])
+    return total_imgs
 
 if __name__ == '__main__':
-	model_path = 'models/tbtest.82-2.39.hdf5'
-	target_size = (224,224)
-	bs = 30
-	steps = 450 // bs
+    os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    # os.environ["CUDA_VISIBLE_DEVICES"]="1"
+    # model_path = '/home/dh/Workspace/aicamp/models/inception_resnet_v2_acc.102-0.922.hdf5'
+    # models_path = '/home/dh/Workspace/aicamp/models/inception_resnet_v2_fulldata'
+    models_path = '/home/dh/Workspace/aicamp/models/inception_resnet_v2_small_competition/'
+    # test_folder = 'data/yoga/test'
+    test_folder = 'data/competition_split/test'
 
-	test_folder = 'data/baseline/test'
-	test_datagen = ImageDataGenerator(rescale=1./255)
-	test_generator = test_datagen.flow_from_directory(
-	        test_folder,
-	        target_size=target_size,
-	        batch_size=bs,
-	        class_mode='categorical')
-	
-	model = load_model(model_path, custom_objects={'relu6':relu6, 'tf':tf})
-
-	results = model.evaluate_generator(test_generator, max_queue_size=bs, steps=steps, verbose=1)
-	for metric, scalar in zip( model.metrics_names, results ):
-		print('{}: {}'.format(metric, scalar))
+    target_size = (224,224)
+    bs = 64
+    # bs = 4
+    total_testimgs = num_jpgs_nested_in_folder( test_folder )
+    steps = total_testimgs // bs
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    test_generator = test_datagen.flow_from_directory(
+            test_folder,
+            target_size=target_size,
+            batch_size=bs,
+            class_mode='categorical')
+  
+    overall_results = {}
+    overall_results['all'] = {}
+    # model = load_model(model_path, custom_objects={'relu6':relu6, 'tf':tf})
+    for model_ in os.listdir(models_path):
+        if not model_.endswith('.hdf5'):
+            continue
+        model_path = os.path.join( models_path, model_ )
+        print(model_path)
+        model = load_model(model_path)
+        results = model.evaluate_generator(test_generator, max_queue_size=bs, steps=steps, verbose=1)
+        for metric, scalar in zip( model.metrics_names, results ):
+            print('{}: {}'.format(metric, scalar))
+            overall_results['all'][model_] = (metric, scalar)
+            if metric not in overall_results:
+                overall_results[metric] = scalar
+            elif overall_results[metric] < scalar:
+                overall_results[metric] = scalar
+        del model
+    print('######### SUMMARY ##########')
+    print(overall_results)
--- train.py
+++ train.py
@@ -10,154 +10,175 @@ from keras.callbacks import ModelCheckpoint, CSVLogger, TensorBoard
 from mobilenetv2 import mobilenetv2
 
 def get_model_0(num_classes, verbose=True):
-	input_img = Input(shape=(224, 224, 3))
+    input_img = Input(shape=(224, 224, 3))
 
-	x = Conv2D(64, (3, 3), padding='same', strides=2)(input_img) # 112,112,64
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
-	x = Conv2D(64, (3, 3), padding='same', strides=2)(x)# 56,56,64
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
+    x = Conv2D(64, (3, 3), padding='same', strides=2)(input_img) # 112,112,64
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(64, (3, 3), padding='same', strides=2)(x)# 56,56,64
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
-	x = Conv2D(128, (3, 3), padding='same', strides=2)(x) # 28,28,128
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
-	x = Conv2D(128, (3, 3), padding='same', strides=2)(x)# 14,14,128
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
+    x = Conv2D(128, (3, 3), padding='same', strides=2)(x) # 28,28,128
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(128, (3, 3), padding='same', strides=2)(x)# 14,14,128
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
-	x = Conv2D(256, (3, 3), padding='same', strides=2)(x) # 7,7,256
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
-	x = Conv2D(256, (3, 3), padding='same', strides=2)(x)# 4,4,256
-	x = Activation('relu')(x)
+    x = Conv2D(256, (3, 3), padding='same', strides=2)(x) # 7,7,256
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(256, (3, 3), padding='same', strides=2)(x)# 4,4,256
+    x = Activation('relu')(x)
 
-	x = GlobalAveragePooling2D()(x)
+    x = GlobalAveragePooling2D()(x)
 
-	predictions = Dense(num_classes, activation='softmax')(x)
+    predictions = Dense(num_classes, activation='softmax')(x)
 
-	model = Model(inputs=input_img, outputs=predictions)
-	model.compile(optimizer='adam',
-	              loss='categorical_crossentropy',
-	              metrics=['accuracy'])
-	if verbose:
-		model.summary()
-	return (224, 224), model
+    model = Model(inputs=input_img, outputs=predictions)
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    if verbose:
+        model.summary()
+    return (224, 224), model
 
 def get_model_1(num_classes, verbose=True):
-	input_img = Input(shape=(224, 224, 3))
+    input_img = Input(shape=(224, 224, 3))
 
-	x = Conv2D(64, (3, 3), padding='same', strides=2)(input_img) # 112,112,64
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
-	x = Conv2D(64, (1, 1), padding='same')(x)# 112,112,64
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
+    x = Conv2D(64, (3, 3), padding='same', strides=2)(input_img) # 112,112,64
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(64, (1, 1), padding='same')(x)# 112,112,64
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
-	x = Conv2D(128, (3, 3), padding='same', strides=2)(x) # 56,56,128
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
-	x = Conv2D(128, (1, 1), padding='same')(x)# 56,56,128
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
+    x = Conv2D(128, (3, 3), padding='same', strides=2)(x) # 56,56,128
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(128, (1, 1), padding='same')(x)# 56,56,128
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
-	x = Conv2D(256, (3, 3), padding='same', strides=2)(x) # 28,28,256
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
-	x = Conv2D(256, (1, 1), padding='same')(x)# 28,28,256
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
+    x = Conv2D(256, (3, 3), padding='same', strides=2)(x) # 28,28,256
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(256, (1, 1), padding='same')(x)# 28,28,256
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
-	x = Conv2D(512, (3, 3), padding='same', strides=2)(x) # 14,14,512
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
-	x = Conv2D(512, (1, 1), padding='same')(x)# 14,14,512
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
+    x = Conv2D(512, (3, 3), padding='same', strides=2)(x) # 14,14,512
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(512, (1, 1), padding='same')(x)# 14,14,512
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
-	x = Conv2D(1024, (3, 3), padding='same', strides=2)(x) # 7,7,1024
-	x = BatchNormalization()(x)
-	x = Activation('relu')(x)
-	x = Conv2D(1024, (1, 1), padding='same')(x)# 7,7,1024
-	x = Activation('relu')(x)
+    x = Conv2D(1024, (3, 3), padding='same', strides=2)(x) # 7,7,1024
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(1024, (1, 1), padding='same')(x)# 7,7,1024
+    x = Activation('relu')(x)
 
-	x = GlobalAveragePooling2D()(x)
+    x = GlobalAveragePooling2D()(x)
 
-	predictions = Dense(num_classes, activation='softmax')(x)
+    predictions = Dense(num_classes, activation='softmax')(x)
 
-	model = Model(inputs=input_img, outputs=predictions)
-	model.compile(optimizer='adam',
-	              loss='categorical_crossentropy',
-	              metrics=['accuracy'])
-	if verbose:
-		model.summary()
-	return (224, 224), model
+    model = Model(inputs=input_img, outputs=predictions)
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    if verbose:
+        model.summary()
+    return (224, 224), model
 
 def get_model_2(num_classes, verbose=True):
-	# from keras.applications.inception_v3 import InceptionV3
-	from keras.applications.inception_resnet_v2 import InceptionResNetV2
-	base_model = InceptionResNetV2(weights='imagenet', include_top=False)
-	x = base_model.output
-	x = GlobalAveragePooling2D()(x)
-	x = Dense(1024, activation='relu')(x)
-	predictions = Dense(num_classes, activation='softmax')(x)
-	model = Model(inputs=base_model.input, outputs=predictions)
+    # from keras.applications.inception_v3 import InceptionV3
+    from keras.applications.inception_resnet_v2 import InceptionResNetV2
+    base_model = InceptionResNetV2(weights='imagenet', include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    predictions = Dense(num_classes, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
 
-	model.compile(optimizer='adam',
-	              loss='categorical_crossentropy',
-	              metrics=['accuracy'])
-	if verbose:
-		model.summary()
-	return (224, 224), model
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    if verbose:
+        model.summary()
+    return (224, 224), model
 
 if __name__ == '__main__':
-	os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    # os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
-	context = 'inception_resnet_v2'
+    # context = 'inception_resnet_v2_small_competition'
+    context = 'inception_resnet_v2_small_competition_moreaug'
 
-	n_epochs = 300
-	bs = 45
-	train_size = 900
-	val_size = 450
-	train_steps = 3 * train_size // bs
-	val_steps = val_size // bs
-	train_folder = 'data/baseline/train'
-	val_folder = 'data/baseline/val'
+    if not os.path.exists( 'models/{}'.format(context) ):
+        os.makedirs( 'models/{}'.format(context) )
 
-	target_size, model = get_model_2(9)
-	# target_size = (224,224)
-	# target_size, model = mobilenetv2(9)
+    n_epochs = 100
+    bs = 64
+    n_classes = 16
+    # train_size = 300
+    # val_size = 150
+    # train_steps = 3 * train_size // bs
+    # val_steps = val_size // bs
+    train_folder = 'data/competition_split/train'
+    # val_folder = 'data/yoga_split/small/val'
 
-	train_datagen = ImageDataGenerator(
-	        rescale=1./255,
-	        shear_range=0.2,
-	        zoom_range=0.2,
-	        horizontal_flip=True)
+    target_size, model = get_model_2(n_classes)
+    # target_size = (224,224)    
+    # target_size, model = mobilenetv2(9)
 
-	test_datagen = ImageDataGenerator(rescale=1./255)
-	# test_datagen = ImageDataGenerator()
 
-	train_generator = train_datagen.flow_from_directory(
-	        train_folder,
-	        target_size=target_size,
-	        batch_size=bs,
-	        class_mode='categorical')
+    # more intense augmentations
+    train_datagen = ImageDataGenerator(
+            rescale=1./255,
+            rotation_range=45,#in deg
+            brightness_range= [0.5,1.5],
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            validation_split=0.2)
+    # train_datagen = ImageDataGenerator(
+    #         rescale=1./255,
+    #         shear_range=0.2,
+    #         zoom_range=0.2,
+    #         horizontal_flip=True,
+    #         validation_split=0.2)
+    # test_datagen = ImageDataGenerator(rescale=1./255)
+    # test_datagen = ImageDataGenerator()
+    val_datagen = ImageDataGenerator(
+            rescale = 1./255,
+            )
 
-	validation_generator = test_datagen.flow_from_directory(
-	        val_folder,
-	        target_size=target_size,
-	        batch_size=bs,
-	        class_mode='categorical')
+    train_generator = train_datagen.flow_from_directory(
+            train_folder,
+            target_size=target_size,
+            batch_size=bs,
+            class_mode='categorical',
+            subset='training')
 
-	csvLogger = CSVLogger('logs/{}.log'.format(context))
-	valLossCP = ModelCheckpoint('models/{}'.format(context) + '_loss.{epoch:02d}-{val_loss:.2f}.hdf5', save_best_only=True)
-	valAccCP = ModelCheckpoint('models/{}'.format(context) + '_acc.{epoch:02d}-{val_acc:.3f}.hdf5', monitor='val_acc', save_best_only=True)
-	tbCallback = TensorBoard( log_dir='./tensorboardlogs', histogram_freq=0, write_graph=True, write_images=True )
+    validation_generator = train_datagen.flow_from_directory(
+            train_folder,
+            target_size=target_size,
+            batch_size=bs,
+            class_mode='categorical',
+            subset='validation')
 
-	model.fit_generator(
-	        train_generator,
-	        steps_per_epoch=train_steps,
-	        epochs=n_epochs,
-	        validation_data=validation_generator,
-	        validation_steps=val_steps,
-	        callbacks=[csvLogger, valLossCP, valAccCP, tbCallback])
+    csvLogger = CSVLogger('logs/{}.log'.format(context))
+    valLossCP = ModelCheckpoint('models/{}/{}'.format(context, context) + '_loss.{epoch:02d}-{val_loss:.2f}.hdf5', save_best_only=True)
+    valAccCP = ModelCheckpoint('models/{}/{}'.format(context, context) + '_acc.{epoch:02d}-{val_acc:.3f}.hdf5', monitor='val_acc', save_best_only=True)
+    tbCallback = TensorBoard( log_dir='./{}_tblogs'.format(context), histogram_freq=0, write_graph=True, write_images=True )
+
+    model.fit_generator(
+            train_generator,
+            steps_per_epoch=train_generator.samples // bs,
+            epochs=n_epochs,
+            validation_data=validation_generator,
+            validation_steps=validation_generator.samples // bs,
+            callbacks=[csvLogger, valLossCP, valAccCP, tbCallback])
