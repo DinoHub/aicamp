4b69663452e5f684f426f5f15ecbfe0b48205ed2
DinoHub <dstadh17@gmail.com>
2019-05-14 22:33:44 +0800

    worked out resnets

--- train.py
+++ train.py
@@ -142,6 +142,9 @@ def get_xception(num_classes, verbose=True):
     return 32, (224, 224), model
 
 def get_resnet152_v2(num_classes, verbose=True):
+    import keras
+    import keras_applications
+    keras_applications.set_keras_submodules(backend=keras.backend, layers=keras.layers,models=keras.models, utils=keras.utils)
     from keras_applications.resnet_v2 import ResNet152V2
     base_model = ResNet152V2(input_shape=(224,224,3), weights='imagenet', include_top=False)
     x = base_model.output
@@ -157,6 +160,25 @@ def get_resnet152_v2(num_classes, verbose=True):
         model.summary()
     return 32, (224, 224), model
 
+def get_resnet101_v2(num_classes, verbose=True):
+    import keras
+    import keras_applications
+    keras_applications.set_keras_submodules(backend=keras.backend, layers=keras.layers,models=keras.models, utils=keras.utils)
+    from keras_applications.resnet_v2 import ResNet101V2
+    base_model = ResNet101V2(input_shape=(224,224,3), weights='imagenet', include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    predictions = Dense(num_classes, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    if verbose:
+        model.summary()
+    return 64, (224, 224), model
+
 def get_model(context, num_classes, verbose=True):
     if context == 'inception_resnet_v2':
         return get_inception_resnet_v2( num_classes, verbose )
@@ -166,6 +188,8 @@ def get_model(context, num_classes, verbose=True):
         return get_xception( num_classes, verbose )
     elif context == 'resnet152_v2':
         return get_resnet152_v2(num_classes, verbose)
+    elif context == 'resnet101_v2':
+        return get_resnet101_v2(num_classes, verbose)
 
 
 if __name__ == '__main__':
@@ -173,7 +197,7 @@ if __name__ == '__main__':
     # os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
     # context = 'inception_resnet_v2_small_competition'
-    contexts = ['xception', 'inception_resnet_v2', 'inception_v3']
+    contexts = ['resnet152_v2', 'resnet101_v2']
 
     n_epochs = 200
     # bs = 32
@@ -183,8 +207,8 @@ if __name__ == '__main__':
     # train_steps = 3 * train_size // bs
     # val_steps = val_size // bs
     train_folder = 'data/TIL2019_v0.1/train'
-    val_folder = 'data/TIL2019_v0.1/val'
-
+    val_folder = 'data/TIL2019_v0.1/test'
+    from keras_applications.resnet_v2 import preprocess_input
     for context in contexts:
         if not os.path.exists( 'models/{}'.format(context) ):
             os.makedirs( 'models/{}'.format(context) )
@@ -193,15 +217,17 @@ if __name__ == '__main__':
 
         # more intense augmentations
         train_datagen = ImageDataGenerator(
-                rescale=1./255,
+                # rescale=1./255,
                 rotation_range=45,#in deg
                 brightness_range= [0.5,1.5],
                 shear_range=0.2,
                 zoom_range=0.2,
-                horizontal_flip=True)
+                horizontal_flip=True,
+                preprocessing_function=preprocess_input)
 
         val_datagen = ImageDataGenerator(
-                rescale = 1./255
+                # rescale = 1./255,
+                preprocessing_function=preprocess_input
                 )
 
         train_generator = train_datagen.flow_from_directory(
