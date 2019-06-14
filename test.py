6bbf674f111e3af8fd99e0c1b894cfb98cdceb4a
DigitalHubRocks <dstadh17@gmail.com>
2019-06-14 15:57:04 +0800

    conducting an orchestra

--- test.py
+++ test.py
@@ -47,15 +47,23 @@ def eval_softmax_vectors(test_folder, preds):
     print('accuracy: {0:.6f}'.format( score / i ))
     return filenames
 
-def ensemble_models(models_path, test_folder, num_classes, use_preds=False):
+def ensemble_models(models_path, test_folder, num_classes, weights=None, use_preds=False):
     total_testimgs = num_jpgs_nested_in_folder( test_folder )
     all_preds = np.zeros( (total_testimgs, num_classes) )
     if use_preds:
-        for pred_file in [npy for npy in os.listdir('predictions') if npy.endswith('.npy')]:
-            pred_fp = os.path.join('predictions', pred_file)
+        default_pred_dir = 'predictions_crops_test_tta5'
+        for pred_file in [npy for npy in os.listdir(default_pred_dir) if npy.endswith('.npy')]:
+            pred_fp = os.path.join(default_pred_dir, pred_file)
+
+            weight = 1.0
+            if weights is not None:
+                print('using weight of {} = {}'.format(pred_file, weights[pred_file]))
+                weight = weights[pred_file]
+
+            print('Using {}'.format(pred_fp))
             this_preds = np.load(pred_fp)
             assert all_preds.shape == this_preds.shape, 'loaded shape of preds does not match the overall required shape'
-            all_preds += this_preds
+            all_preds += this_preds * weight
     else:
         class_labels = sorted( list(os.listdir(test_folder)) )
 
@@ -109,7 +117,22 @@ def custom_loss(ytrue, ypred):
     return alpha*K.categorical_crossentropy(ytrue[:,:num_classes], ypred) + (1. - alpha)*K.categorical_crossentropy(ytrue[:,num_classes:], ypred)
 keras.losses.custom_loss = custom_loss
 
-def eval_models_singly(models_path, test_folder, num_classes, save_preds=True):
+def tta(image, pp_fn, model):
+    datagen = ImageDataGenerator(
+            preprocessing_function=pp_fn, 
+            # rotation_range=45,#in deg
+            brightness_range= [0.5,1.5],
+            # shear_range=0.2,
+            # zoom_range=0.2,
+            horizontal_flip=True)
+    samples = np.expand_dims(image, 0)
+    it = datagen.flow(samples, batch_size=5)
+    yhats = model.predict_generator(it, steps=5, verbose=0)
+    summed = np.sum(yhats, axis=0)
+    summed /= sum(summed)
+    return [summed]
+
+def eval_models_singly(models_path, test_folder, num_classes, save_preds=True, use_tta=True, preds_dir='predictions'):
     class_labels = sorted( list(os.listdir(test_folder)) )
 
     target_size = (224,224)
@@ -117,6 +140,9 @@ def eval_models_singly(models_path, test_folder, num_classes, save_preds=True):
 
     # model = load_model(model_path, custom_objects={'relu6':relu6, 'tf':tf})
     model_list = list(os.listdir(models_path))
+    if save_preds:
+        if not os.path.isdir(preds_dir):
+            os.makedirs(preds_dir)
     for model_ in model_list:
         if not model_.endswith('.hdf5'):
             continue
@@ -137,14 +163,17 @@ def eval_models_singly(models_path, test_folder, num_classes, save_preds=True):
                 img_path = os.path.join( pose_folder, im )
                 img = image.load_img(img_path, target_size=target_size)
                 x = image.img_to_array(img)
-                x = np.expand_dims(x, axis=0)
-                x = finder(context, verbose=False)(x)
-                preds = model.predict( x )
+                if use_tta:
+                    preds = tta( x, finder(context, verbose=False), model )
+                else:
+                    x = np.expand_dims(x, axis=0)
+                    x = finder(context, verbose=False)(x)
+                    preds = model.predict( x )
                 all_preds[i] += preds[0]
                 i+=1
         eval_softmax_vectors(test_folder, all_preds)
         if save_preds:
-            np.save( 'predictions/{}.npy'.format(context), all_preds )
+            np.save( os.path.join(preds_dir,'{}.npy'.format(context)), all_preds )
         del model
 
 def conf_mat():
@@ -161,7 +190,7 @@ if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"]="0"
     models_path = sys.argv[1]
     test_folder = sys.argv[2]
-    num_classes = 6
+    num_classes = 15
     # models_path = 'models/best_models'
     # test_folder = 'data/TIL2019_v0.1/test'
     eval_models_singly(models_path, test_folder, num_classes)
